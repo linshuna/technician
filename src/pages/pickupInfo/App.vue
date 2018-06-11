358f629086f6d41b24094ae21c9122bb02afd7aa
hu <455395032@qq.com>
2018-06-11 14:26:34 +0800

    push

---
+++ src/pages/pickupInfo/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="pickup">
+      <div class="title">接车事项</div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'App',
+    data () {
+      return {
+        value: ''
+      }
+    },
+    computed: {
+      
+    },
+    created() {
+      
+    },
+    methods: {
+      
+    },
+    components: {
+      
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~modules/css/variable.styl'
+
+  #app
+    color: #2c3e50
+    font-size: .28rem
+</style>
